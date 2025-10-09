d032c410fd39d0238e30df5224e744f51dced521
s.ryousuke1 <s.ryousuke1@HL01916.local>
2025-10-09 18:34:00 +0900

    runtime bun

--- packages/db/prisma/migrations/20251007074041_users/migration.sql
+++ packages/db/prisma/migrations/20251007074041_users/migration.sql
@@ -1,6 +1,9 @@
 -- CreateTable
-CREATE TABLE "users" (
+CREATE TABLE "tasks" (
     "user_id" TEXT NOT NULL,
+    "task_id" TEXT NOT NULL,
+    "content" VARCHAR(1000) NOT NULL,
+    "completed_at" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
