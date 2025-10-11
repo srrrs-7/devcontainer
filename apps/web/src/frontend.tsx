45d0e101f1652736622bd59d3f85ffbcb66e481e
sRRRs <srrrs4510@gmail.com>
2025-10-11 19:13:57 +0900

    chore: edit github action

--- apps/web/src/frontend.tsx
+++ apps/web/src/frontend.tsx
@@ -9,7 +9,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-const elem = document.getElementById("root")!;
+const elem = document.getElementById("root");
+if (!elem) {
+  throw new Error("Root element not found");
+}
+
 const app = (
   <StrictMode>
     <App />
@@ -18,7 +22,10 @@ const app = (
 
 if (import.meta.hot) {
   // With hot module reloading, `import.meta.hot.data` is persisted.
-  const root = (import.meta.hot.data.root ??= createRoot(elem));
+  if (!import.meta.hot.data.root) {
+    import.meta.hot.data.root = createRoot(elem);
+  }
+  const root = import.meta.hot.data.root;
   root.render(app);
 } else {
   // The hot module reloading API is not available in production.
