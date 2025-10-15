37ee9ef9c85a15194315c89b100c9b841fc53d8d
s.ryousuke1 <s.ryousuke1@HL01916.local>
2025-10-15 15:00:17 +0900

    add vitest

--- apps/api/__test__/setup.sh
+++ apps/api/__test__/setup.sh
@@ -1,20 +1,20 @@
 /**
- * `@chax-at/transactional-prisma-testing`のセットアップ
- * @see https://www.npmjs.com/package/@chax-at/transactional-prisma-testing
+ * テストセットアップ
  *
- * 各テストケースに対して分離されたトランザクションを提供します。
+ * 各テストケースの前後でデータベースをクリーンアップします。
  * これにより、各テストケースは他のテストケースの影響を受けずにデータベース操作を実行できます。
- * テストケース内で発生したすべてのデータベース操作は、テストケースの終了時にロールバックされます。
  */
-import { PrismaTestingHelper } from "@chax-at/transactional-prisma-testing";
 import { getPrisma } from "@packages/db";
 
 type PrismaClient = ReturnType<typeof getPrisma>;
 
-let prismaTestingHelper: PrismaTestingHelper<PrismaClient>;
-
 declare global {
   var testPrismaClient: PrismaClient;
+}
+
+// テスト用のPrismaクライアントを初期化
+if (!global.testPrismaClient) {
+  global.testPrismaClient = getPrisma();
 }
 
 vi.doMock("@packages/db", () => {
@@ -23,16 +23,7 @@ vi.doMock("@packages/db", () => {
   };
 });
 
-beforeEach(async () => {
-  if (prismaTestingHelper == null) {
-    const originalPrisma = getPrisma();
-    prismaTestingHelper = new PrismaTestingHelper(originalPrisma);
-    global.testPrismaClient = prismaTestingHelper.getProxyClient();
-  }
-
-  await prismaTestingHelper.startNewTransaction();
-});
-
 afterEach(async () => {
-  prismaTestingHelper?.rollbackCurrentTransaction();
+  // 各テスト後にすべてのテーブルをクリーンアップ
+  await global.testPrismaClient.tasks.deleteMany({});
 });
